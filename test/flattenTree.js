645f0b169e2d69a90cb7f5a3de9166485a54c247
Karl O'Keeffe <karl@geckoboard.com>
2017-06-04 16:26:37 +0100

    Add failing test

--- test/flattenTree.js
+++ test/flattenTree.js
@@ -1,3 +1,9 @@
 const test = require('ava');
+const exampleQuestionSet = require('../examples/questionset.json');
+const exampleAnswers = require('../examples/answers.json');
+const exampleOutput = require('../examples/expectedOutput.json');
+const flattenTree = require('../lib/flattenTree');
 
-test.todo('flattens correctly the provided examples');
+test('flattens correctly the provided examples', t => {
+  t.deepEqual(flattenTree(exampleQuestionSet, exampleAnswers), exampleOutput);
+});
