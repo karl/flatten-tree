e4c4d3da4a1767dbabfd67a0111c9e5a96e2ff52
Karl O'Keeffe <karl@geckoboard.com>
2017-06-04 16:50:09 +0100

    Add some edge case tests

--- test/flattenTree.js
+++ test/flattenTree.js
@@ -7,3 +7,33 @@ const flattenTree = require('../lib/flattenTree');
 test('flattens correctly the provided examples', t => {
   t.deepEqual(flattenTree(exampleQuestionSet, exampleAnswers), exampleOutput);
 });
+
+test('returns empty array when there are no questions', t => {
+  t.deepEqual(flattenTree([], exampleAnswers), []);
+});
+
+test('only returns root questions when there are no answers', t => {
+  const onlyRootQuestions = [
+    {
+      id: 'question1',
+      type: 'text',
+      text: 'Please answer this question',
+    },
+    {
+      id: 'question2',
+      type: 'number',
+      text: 'Please answer this other question',
+    },
+    {
+      id: 'question3',
+      type: 'yesno',
+      text: 'Let us know about this',
+    },
+    {
+      id: 'question4',
+      type: 'yesno',
+      text: 'Let us know about something else',
+    },
+  ];
+  t.deepEqual(flattenTree(exampleQuestionSet, []), onlyRootQuestions);
+});
